060351285a8dbcec571685cd12b347008a9b87f6
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-13 22:35:13 +0100

    Aula22 - PHP + MySQL - Formulário Parte 1

--- Aula22/index.php
+++ Aula22/index.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>sistema de registo</title>
+    <title>Sistema de registo</title>
 </head>
 <body>
     <?php
-        echo "<p>Olá Mundo</p>";
+        echo "Olá Mundo";        
     ?>
 </body>
 </html>

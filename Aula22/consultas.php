e338011b1c6442a1d2576e4ebb25b4e32d981c5b
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-14 19:20:34 +0100

    Aula24 - PHP e MySQL - Sistema de Consulta Parte 1

---
+++ Aula22/consultas.php
@@ -0,0 +1,32 @@
+<?php
+    include_once("conecao.php")
+?>
+
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de registo!</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+ <div class="container">
+    
+    <nav>
+        <ul class="menu">
+            <a href="index.php"><li>Registo</li></a>
+            <a href="consultas.php"><li>Consultas</li></a>
+        </ul>
+    </nav>
+    
+    <section>
+        <h1>Consultas</h1>
+        <hr><br><br>
+
+
+    </section>
+ </div>
+    
+</body>
+</html>

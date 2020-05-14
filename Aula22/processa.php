2c8e123934a4555a64d69864b39f5cff784f97fe
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-14 19:10:39 +0100

    Aula24 - PHP e MySQL - Sistema de Consulta Parte 1

--- Aula22/processa.php
+++ Aula22/processa.php
@@ -9,3 +9,31 @@
     $salvar = mysqli_query($conexao, $sql);
     mysqli_close($conexao)
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de registo!</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+ <div class="container">
+    
+    <nav>
+        <ul class="menu">
+            <a href="index.php"><li>Registo</li></a>
+            <a href="consultas.php"><li>Consultas</li></a>
+        </ul>
+    </nav>
+    
+    <section>
+        <h1>Confirmação de Registo</h1>
+        <hr><br><br>
+
+    </section>
+ </div>
+    
+</body>
+</html>

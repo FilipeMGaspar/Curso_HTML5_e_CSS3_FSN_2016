7ba3d3d1cf1f08ab2c80a37e47d26ab62ca6465f
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-14 18:45:52 +0100

    Aula22 - PHP e MySQL - Criando um Sistema Básico de Cadastro

--- Aula22/conecao.php
+++ Aula22/conecao.php
@@ -1,13 +1,13 @@
 <?php
 
     /*Conecção ao servidor de base de dados*/
-    $hostname = "localhost ";
+    $hostname = "localhost";
     $user= "root";
     $password = "";
     $database = "registo";
-    $conexao = mysqli($hostname, $user, $password, $database);
+    $conexao = mysqli_connect($hostname, $user, $password, $database);
 
-    if(!$conexaocone){
+    if(!$conexao){
         print "Flha na conecção com a Base de dados";
     }
 ?>
--- Aula22/processa.php
+++ Aula22/processa.php
@@ -1,5 +1,5 @@
 <?php
-    include_once("conexao.php");
+    include_once("conecao.php");
 
     mysqli_close($conexao)
 ?>

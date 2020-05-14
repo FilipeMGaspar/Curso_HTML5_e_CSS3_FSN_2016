a9a44d860a8931b828051e625595f23dc76a5444
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-14 17:10:38 +0100

    Aula22 - PHP e MySQL - Criando um Sistema Básico de Cadastro

--- Aula22/index.php
+++ Aula22/index.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de registo</title>
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
-    <?php
-        echo "<h1>Olá Mundo</h1>"; 
-    ?>
+ <div class="container">
+    <nav>
+    </nav>
+    <section>
+    </section>
+ </div>
+    
 </body>
 </html>

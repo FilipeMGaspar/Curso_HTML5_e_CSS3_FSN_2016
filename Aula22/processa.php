33519f12be0a821282ab760551b964319e0a255e
Filipe Miguel Martins Gaspar <62749595+FilipeMGaspar@users.noreply.github.com>
2020-05-14 18:59:36 +0100

    Aula23 -PHP e MySQL - Sistema de Cadastro

--- Aula22/processa.php
+++ Aula22/processa.php
@@ -1,5 +1,9 @@
 <?php
     include_once("conecao.php");
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $profissao = $_POST['profissao'];
 
+    $sql = "insert into utilizadores ()"
     mysqli_close($conexao)
 ?>
